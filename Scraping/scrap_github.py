49888f1a42086f774db5db956ba1c088f32f00ea
FredCar <carricart.f@gmail.com>
2021-02-10 20:08:15 +0100

    remove multilines comments and chinese characters from the scraper script

--- Scraping/scrap_github.py
+++ Scraping/scrap_github.py
@@ -13,7 +13,17 @@ start_time = time.time()
 def code_cleaner(code, nb_lines=0):
     lines = code.splitlines()
     clean_code = ""
+    multilines_comment = False
+
     for line in lines:
+
+        # Si on est dans un commentaire multilignes
+        if multilines_comment:
+            # On regarde si c'est la fin du commentaire
+            if re.search(r"(\"{3}|'{3}) *$", line) != None:
+                multilines_comment = False
+            continue
+            
         # On supprime les sauts de lignes
         if re.match(r"^ *$", line) == None:
             # On supprime les lignes de commentaires
@@ -23,9 +33,26 @@ def code_cleaner(code, nb_lines=0):
                     # Dièse dans une string "#"
                     # Dièse échapé \#
                 line = re.sub(r"#.*$", " ", line)
-                # TODO Supprimer les caractères étrangés
-                clean_code += f"{line}\n"
-                nb_lines += 1
+
+        # On supprime les commentaires multilignes """Texte..."""
+        if re.match(r"^[ \t]*(\"{3}|'{3}).*$", line) != None:
+            # Si le commentaire ne se termine pas sur la même ligne
+            if re.match(r"^.*(\"{3}|'{3}) *$", line) == None:
+                multilines_comment = True
+            continue
+
+        # On supprime les caractères Chinois
+        if re.search(r"\\p{Han}", line) != None:
+            continue
+
+        # On supprime les caractères non utf-8
+        # if re.search(r"([\x00-\x7F]|[\xC2-\xDF][\x80-\xBF]|\xE0[\xA0-\xBF][\x80-\xBF]|[\xE1-\xEC][\x80-\xBF]{2}|\xED[\x80-\x9F][\x80-\xBF]|[\xEE-\xEF][\x80-\xBF]{2}|\xF0[\x90-\xBF][\x80-\xBF]{2}|[\xF1-\xF3][\x80-\xBF]{3}|\xF4[\x80-\x8F][\x80-\xBF]{2})*", line) != None:
+        #     print(">>>", line)
+        #     continue
+
+
+        clean_code += f"{line}\n"
+        nb_lines += 1
 
     return clean_code, nb_lines
 
