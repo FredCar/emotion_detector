9395ad4a501ffd551cfc9b6b5f70be85deb0015b
FredCar <carricart.f@gmail.com>
2021-02-28 19:31:03 +0100

    scrap: put functions in another file and start implementing the function extractor

--- Scraping/scrap_github.py
+++ Scraping/scrap_github.py
@@ -1,79 +1,16 @@
-import re
 import time
 import requests
 import traceback
 from github import Github
+from utils.functions import *
 
 
 g = Github("7629f4960da628371d797c312ab3f3e327b45120") # Personal Github token
-language = "python" # What language we search on Github
 start_time = time.time()
 
-
-def code_cleaner(code, nb_lines=0):
-    lines = code.splitlines()
-    clean_code = ""
-    multilines_comment = False
-
-    for line in lines:
-
-        # Si on est dans un commentaire multilignes
-        if multilines_comment:
-            # On regarde si c'est la fin du commentaire
-            if re.search(r"(\"{3}|'{3}) *$", line) != None:
-                multilines_comment = False
-            continue
-            
-        # On supprime les sauts de lignes
-        if re.match(r"^ *$", line) != None:
-            continue
-
-        # On supprime les lignes de commentaires
-        if re.match(r"^[\t ]*#.*$", line) != None:
-            continue
-
-        # On supprime les espace en fin de ligne
-        line = re.sub(r" +$", "", line)
-
-        # On supprime les commentaires de fin de lignes
-        # TODO Gérer les exception :
-            # Dièse dans une string "#"
-            # Dièse échapé \#
-        line = re.sub(r"#.*$", "", line)
-
-        # On supprime les commentaires multilignes """Texte..."""
-        if re.match(r"^[ \t]*(\"{3}|'{3}).*$", line) != None:
-            # Si le commentaire ne se termine pas sur la même ligne
-            if re.match(r"^.*(\"{3}|'{3}) *$", line) == None:
-                multilines_comment = True
-            continue
-
-        # On supprime les caractères Chinois
-        if re.search(r"\\p{Han}", line) != None:
-            continue
-
-        # On supprime les caractères non utf-8
-        # if re.search(r"([\x00-\x7F]|[\xC2-\xDF][\x80-\xBF]|\xE0[\xA0-\xBF][\x80-\xBF]|[\xE1-\xEC][\x80-\xBF]{2}|\xED[\x80-\x9F][\x80-\xBF]|[\xEE-\xEF][\x80-\xBF]{2}|\xF0[\x90-\xBF][\x80-\xBF]{2}|[\xF1-\xF3][\x80-\xBF]{3}|\xF4[\x80-\x8F][\x80-\xBF]{2})*", line) != None:
-        #     print(">>>", line)
-        #     continue
-
-
-        clean_code += f"{line}\n"
-        nb_lines += 1
-
-    return clean_code, nb_lines
-
-
-def timer(s):
-    h, m = 0, 0
-    if s > 60:
-        m = s // 60
-        s = s % 60
-    if m > 60:
-        h = m // 60
-        m = m % 60
-
-    return f"{int(h):02d}:{int(m):02d}:{int(s):02d}"
+language = "python" # What language we search on Github
+file_name = "code_dataset_test.txt"
+scrap_functions = True
 
 
 def main():
@@ -112,10 +49,17 @@ def main():
                         # Nettoyage (suppression des commentaires et des sauts de ligne)
                         code, nb_lines = code_cleaner(code, nb_lines)
 
-                        # On enregistre le code dans un fichier
-                        text = f"{code}\n"
-                        with open("../Data/code_dataset_test.txt", "a") as file:
-                            file.write(text)
+                        if scrap_functions:
+                            # TODO (WIP)
+                            functions = extract_functions(code)
+                            text = f"{code}\n"
+                            with open(f"../Data/functions/{file_name}", "w") as file:
+                                file.write(text)
+                        else:
+                            # On enregistre le code dans un fichier
+                            text = f"{code}\n"
+                            with open(f"../Data/{file_name}", "w") as file:
+                                file.write(text)
         except:
             print("\n\tERROR =======================")
             traceback.print_exc()
