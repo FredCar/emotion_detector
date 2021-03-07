5b3e5e6395b85e1648f91261c2e43dd73b74bbb3
FredCar <carricart.f@gmail.com>
2021-03-07 19:10:15 +0100

    front : fetch data from API

---
+++ Front/src/Routing.js
@@ -0,0 +1,5 @@
+const Routing = {
+    baseUrl: "http://127.0.0.1:5000",
+}
+
+export default Routing;
--- Front/src/components/SubmitForm.jsx
+++ Front/src/components/SubmitForm.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import Routing from "../Routing";
+import axios from 'axios';
 
 const SubmitForm = ((props) => {
+    const [test, setTest] = useState();
+
+
+    const fetcher = (() => {
+            axios.get(Routing.baseUrl)
+            .then((data) => {
+                setTest(data.data.message)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    });
+
+    useEffect(() => {
+        fetcher();
+    }, [])
+
+
     return (
         <>
             <form>
-            
+
                 <textArea 
                     placeholder="Entrez votre code içi..." 
                     cols="130" 
@@ -22,6 +43,10 @@ const SubmitForm = ((props) => {
                 />
 
             </form>
+
+            <h3>TEST</h3>
+            <p>{test}</p>
+
         </>
     )
 });
