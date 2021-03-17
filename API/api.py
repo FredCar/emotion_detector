9e13621655510293ee3cc96ce7a8c264a91d26a9
FredCar <carricart.f@gmail.com>
2021-03-17 19:24:06 +0100

    api : start SQLAlchemy initialization

--- API/api.py
+++ API/api.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
+from utils import config
 
 app = Flask(__name__)
 
 
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{config.MYSQL_USER}:{config.MYSQL_PASSWORD}@database/code_generator_db'
 
 
 @app.route("/", methods=["GET"])
