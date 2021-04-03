8b0295c9a3b07052e6f0b886e175af384c6a0b2f
FredCar <carricart.f@gmail.com>
2021-04-03 19:09:52 +0200

    api : (wip) implement the model

--- API/api.py
+++ API/api.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
+import tensorflow as tf
+from transformers import TFBertForSequenceClassification
+from transformers import BertTokenizer
 from utils import config
 
 
@@ -22,6 +25,16 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+
+model_save_path = ""
+num_labels = 2
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+optimizer = tf.keras.optimizers.Adam(learning_rate=2e-5,epsilon=1e-08)
+metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
+model = TFBertForSequenceClassification.from_pretrained('bert-base-uncased',num_labels=num_labels)
+model.compile(loss=loss,optimizer=optimizer, metrics=[metric])
+model.load_weights(model_save_path)
 
 
 @app.route("/", methods=["GET", "POST"])
