fb1a58502307659b26da3244c1bba15eb02ae8d8
FredCar <carricart.f@gmail.com>
2021-04-03 16:45:22 +0200

    front : add a navbar from Bootstrap

--- Front/src/components/Menu.jsx
+++ Front/src/components/Menu.jsx
@@ -3,7 +3,21 @@ import React from "react";
 const Menu = ((props) => {
     return (
         <>
-            <h1>Welcome to the Code Generator.</h1>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">Détecteur d'émotions</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+            <h1>Welcome to the Emotions Detector.</h1>
         </>
     )
 });
