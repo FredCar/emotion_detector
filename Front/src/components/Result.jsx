410d24ca062074593579aabf4b20a029ed1cd1ae
FredCar <carricart.f@gmail.com>
2021-04-11 19:59:51 +0200

    front : add emojis

--- Front/src/components/Result.jsx
+++ Front/src/components/Result.jsx
@@ -1,10 +1,61 @@
 import React from "react";
 
+const BestResult = ({bestResult}) => {
+    console.log("BestResult", bestResult)
+    let emoji = ""
+
+    switch (bestResult) {
+        case "amour":
+            emoji = "emoji_love.png"
+            break
+        case "colère":
+            emoji = "emoji_anger.png"
+            break
+        case "joie":
+            emoji = "emoji_joy.png"
+            break
+        case "peur":
+            emoji = "emoji_fear.png"
+            break
+        case "surprise":
+            emoji = "emoji_surprise.png"
+            break
+        case "tristesse":
+            emoji = "emoji_sadness.png"
+            break
+
+    }
+    return (
+        <>
+            <img src={`images/${emoji}`}  />
+        </>
+    )
+}
+
 const Result = ((props) => {
-    // let sessionStorage  = window.sessionStorage;
+    const data = JSON.parse(sessionStorage.getItem("data"))
+    // data["all_results"] = JSON.parse(data["all_results"])
+
+    // console.log("data", typeof data["all_results"])
+
+    for (const res in data["all_results"]) {
+        console.log("res", res, data["all_results"][res])
+    }
+
 
     return (
-        <h1>{sessionStorage.getItem("result")}</h1>
+        <>
+            <BestResult bestResult={data["best_result"]}/>
+            <br />
+            <h2>{data["best_result"]}</h2>
+            <h2>{data["original_text"]}</h2>
+            <h2>{data["translated_text"]}</h2>
+            <h2>{data["sents"]}</h2>
+            {/* {data["all_results"].forEach((key) => {
+                return (<h4>{key}</h4>)
+            })} */}
+            <p>{data["all_results"]["amour"]}</p>
+        </>
     )
 })
 
