8318425ac67ddc920c47b3214c8c4fac14e4dffa
FredCar <carricart.f@gmail.com>
2021-04-13 14:48:36 +0200

    front : improve ResultsTable

--- Front/src/components/ResultsTable.jsx
+++ Front/src/components/ResultsTable.jsx
@@ -1,56 +1,70 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 const useStyle = makeStyles({
-    table: {
-        margin: "auto",
-        border: "1px solid red",
-    },
-    tr: {
-        border: "1px solid red",
-    },
-    th: {
-        border: "1px solid red",
-    },
-    td: {
-        border: "1px solid red",
-    },
+    // table: {
+    //     margin: "auto",
+    //     border: "1px solid red",
+    // },
+    // tr: {
+    //     border: "1px solid red",
+    // },
+    // th: {
+    //     border: "1px solid red",
+    // },
+    // td: {
+    //     border: "1px solid red",
+    // },
 })
 
 const ResultsTable = ({detailedResults, phrases}) => {
     const classes = useStyle();
 
-    console.log("phrases", phrases)
-
     let headers = []
-    let rows = []
-    for (const res in detailedResults) {
-        headers.push(<th className={classes.th} >{res}</th>)
+    for (const res in detailedResults[Object.keys(detailedResults)[0]]) {
+        headers.push(<TableCell className={classes.th} >{res}</TableCell>)
     }
-
-    for (let i=0; i < phrases.length; i++ ) {
+    
+    let rows = []
+    for (let i = 0; i < phrases.length; i++ ) {
         let column = []
-        column.push(
-
-        )
+        for (const emotion in detailedResults[phrases[i]]) {
+            console.log("emotion", emotion)
+            column.push(
+                <TableCell>{detailedResults[phrases[i]][emotion]}</TableCell>
+            )
+        }
 
         rows.push(
-            <tr>
-                <td className={classes.td} >{phrases[i]}</td>
+            <TableRow>
+                <TableCell className={classes.td} ><strong>{phrases[i]}</strong></TableCell>
                 {column}
-            </tr>
+            </TableRow>
         )
     }
 
     return (
         <>
-            <table className={classes.table} >
-                <tr className={classes.tr} >
-                    <th className={classes.th} >Phrases</th>
-                    {headers}
-                </tr>
-                {rows}
-            </table>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} >
+                    <TableHead>
+                        <TableRow className={classes.tr} >
+                            <TableCell className={classes.th} >Phrases</TableCell>
+                            {headers}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </>
     )
 };
