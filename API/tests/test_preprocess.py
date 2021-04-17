c3c1e43e71b5d20363594a33beb719ebcc088034
FredCar <carricart.f@gmail.com>
2021-04-17 02:11:16 +0200

    api : add tests for Preprocess class

--- API/tests/test_preprocess.py
+++ API/tests/test_preprocess.py
@@ -11,3 +11,18 @@ def test_Preprocess_class_init():
     assert type(p) == Preprocess
     assert type(p.translator) == Translator
     assert type(p.tokenizer) == BertTokenizer
+
+
+def test_translate_function():
+    p = Preprocess()
+    sent = "Bonjour tout le monde"
+    result = p.translate(sent)
+    assert result == "Hello everybody"
+
+
+def test_tokenize_function():
+    p = Preprocess()
+    sents = ["Je vais à la plage", "Bonjour tout le monde"]
+    result = p.tokenize(sents)
+    assert len(result) == 2
+    assert list(result[0].keys()) == ['input_ids', 'token_type_ids', 'attention_mask']
