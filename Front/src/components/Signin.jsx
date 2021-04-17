da2d4b75673af1df95b8c319d47fc354f50b09ff
FredCar <carricart.f@gmail.com>
2021-04-17 23:15:50 +0200

    front : add Signin form

--- Front/src/components/Signin.jsx
+++ Front/src/components/Signin.jsx
@@ -1,8 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import Alert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyle = makeStyles({
+  submitButton: {
+      width: 485,
+  },
+  progressBar: {
+      width: 485,
+      margin: "auto",
+  },
+  alert: {
+      width: 800,
+      margin: "auto",
+      marginBottom: 20,
+  },
+});
 
 const Signin = (props) => {
+    const classes = useStyle();
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [confirmPassword, setConfirmPassword] = useState();
+    const [alert, setAlert] = useState();
+
+    const handleSubmit = () => {
+        if (password != confirmPassword) {
+            setAlert("Les mots de passe sont différents !")
+        }
+    }
+
     return (
-        <h1>Sign in</h1>
+        <>
+            {
+                alert && <>
+                <Alert severity="error" className={classes.alert} >
+                    {alert}
+                </Alert>
+                </>
+            }
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Nom : 
+                    <input type="texte" onChange={(e) => setName(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    Email : 
+                    <input type="email" onChange={(e) => setEmail(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    Mot de passe : 
+                    <input type="password" onChange={(e) => setPassword(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    Confirmation du mot de passe : 
+                    <input type="password" onChange={(e) => setConfirmPassword(e.target.value)} />
+                </label>
+                <br />
+                <input type="submit" value="Envoyer" />
+            </form>
+        </>
     )
 };
 
