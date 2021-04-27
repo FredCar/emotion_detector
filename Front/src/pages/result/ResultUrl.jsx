c33aeb4fdbd915bcc66e62621c74456327bef028
FredCar <carricart.f@gmail.com>
2021-04-27 04:48:15 +0200

    front : (wip) add components to ResultUrl

--- Front/src/pages/result/ResultUrl.jsx
+++ Front/src/pages/result/ResultUrl.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import BestResult from "./BestResult";
+import ResultsTable from "./ResultsTable";
 
 const ResultUrl = (props) => {
+    const data = JSON.parse(sessionStorage.getItem("data"))
+
     return (
-        <h1>URL</h1>
+        <>
+            <h1>URL</h1>
+            <BestResult bestResult={data["best_result"]}/>
+            <br /> <br />
+            <ResultsTable detailedResults={data["detailed_results"]} phrases={data["phrases"]} />
+        </>
     )
 };
 
