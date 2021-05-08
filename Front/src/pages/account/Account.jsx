cd19ffd68a7b9ebcdd29a10b1bc324dd55659ebe
FredCar <carricart.f@gmail.com>
2021-05-08 21:45:46 +0200

    Front : add fetchData function

--- Front/src/pages/account/Account.jsx
+++ Front/src/pages/account/Account.jsx
@@ -1,4 +1,21 @@
 import React from "react";
+import axios from "axios";
+import Routing from "../../Routing";
+
+const fetchData = () => {
+    let url = `${Routing.baseUrl}/account`
+    let config = {
+        headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` }
+    };
+
+    axios.get(url, config)
+    .then((data) => {
+        console.log("DATA", data)
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+}
 
 const Account = (props) => {
     return (
